e728f06c2cbde7ccf91333e89cccc2a1b599d148
alanredsheep <35416678+alanredsheep@users.noreply.github.com>
2018-04-17 21:52:31 +0800

    TimeTest

---
+++ TEST/TimeTest/Timetest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace TimeTest
+{
+    class Timetest
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+
+
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed);
+
+            Console.ReadKey();
+        }
+    }
+}
